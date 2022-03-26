3062e6e7d5262391586260c699adc5fd18c616ea
mraihanhatim <raihan04042001@gmail.com>
2022-03-26 23:36:57 +0700

    Menerapkan Fungsi Fetch

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,16 +1,7 @@
-import clubs from './clubs.js';
-
 class DataSource {
     static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club => club.name.toUpperCase().includes(keyword.toUpperCase()));
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found"`);
-            }
-        });
+        return fetch(`https://sports-api.dicoding.dev/teams/search?t=${keyword}`)
     }
-}
-
-export default DataSource;
+ }
+  
+ export default DataSource;
